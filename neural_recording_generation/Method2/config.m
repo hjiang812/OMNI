92b21c3b3ca73819e56c09ffbafe64825eb3c9b1
hjiang812 <111116603+hjiang812@users.noreply.github.com>
2026-02-14 11:23:46 +0800

    Add files via upload

---
+++ neural_recording_generation/Method2/config.m
@@ -0,0 +1,13 @@
+%% This file sets the basic parameters used in the data generation process
+
+Params.SNR_target = 10; % Signal-to-noise ratio (SNR) of the generated signal
+
+Params.noiseType = 'gaussian'; % Type of noise
+
+Params.numChannels = 1024;
+
+Params.sample_frequency = 30000; % Signal sampling frequency
+
+Params.length = 1; % Signal duration (in seconds)
+
+Params.influenceRange = 30;  % Influence range of a single neuron
---
+++ neural_recording_generation/Method2/main.m
@@ -0,0 +1,6 @@
+
+config;
+
+generate_recording('templates_cell.mat', 'probe_coords_32x32.mat', 'synthetic_data.mat', Params);
+
+visualize_template('templates_cell.mat');
